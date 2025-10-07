c43d9121b5fc9fca3124dba72a99927c659b7fa4
Maciej W <Maciejaw4@gmail.com>
2025-10-07 20:59:38 +0200

    choinka

---
+++ Choinka.java
@@ -0,0 +1,10 @@
+public class Choinka{
+	public static void main(String[] args){
+
+		for(int i;i<(int)args[0];i++){
+			for (int j; j<i;j++){
+				System.out.print('*');
+				}
+			System.out.println('\n'); }
+}
+}
